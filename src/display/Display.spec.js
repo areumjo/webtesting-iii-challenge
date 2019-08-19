5dbf3104876347b636320f1e61d391e53a771ce5
Areum Jo <areumjo1@gmail.com>
2019-08-19 16:30:27 -0400

    Added snaphot testing and props and rendering testing

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,17 @@
 // Test away!
+import React from 'react';
+import Display from './Display';
+
+import { render, fireEvent } from '@testing-library/react';
+
+
+describe('<Display />', () => {
+    it('Is gate open?', () => {
+        const { getByText } = render(<Display />);
+        expect(getByText('Open'))
+    })
+    it('Locked gate?', () => {
+        const { getByText } = render(<Display locked={true} />);
+        expect(getByText('Locked'));        
+    })
+})
