df07355b87aa25396182bef8d7d775ecd2443b8f
Areum Jo <areumjo1@gmail.com>
2019-08-19 17:03:02 -0400

    Check buttons on display component

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,18 @@
 // Test away
+import React from 'react';
+import { render } from '@testing-library/react';
+import Dashboard from './Dashboard';
+
+describe('<Dashboard /', () => {
+    const { queryByText } = render(<Dashboard />);
+    it('Gate display?', () => {
+        const closeBtn = queryByText('Close Gate');
+        expect(closeBtn);
+        const openBtn = queryByText('Open Gate');
+        expect(openBtn);
+        const lockedBtn = queryByText('Lock Gate');
+        expect(lockedBtn);
+        const unlockedBtn = queryByText('Unlock Gate');
+        expect(unlockedBtn);
+    })
+})
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -6,12 +6,23 @@ import { render, fireEvent } from '@testing-library/react';
 
 
 describe('<Display />', () => {
-    it('Is gate open?', () => {
+    it('Default to unlocked and open?', () => {
         const { getByText } = render(<Display />);
-        expect(getByText('Open'))
+        expect(getByText('Open'));
+        expect(getByText('Unlocked'));
     })
     it('Locked gate?', () => {
         const { getByText } = render(<Display locked={true} />);
-        expect(getByText('Locked'));        
+        expect(getByText('Locked'));  
+        //expect(getByText('Locked')).toHaveClass('locked')
+      
     })
+    it('Closed gate?', () => {
+        const { getByText } = render(<Display closed={true} />);
+        expect(getByText('Closed'));        
+    })
+    // it('locked or closed is red-led?', () => {
+    //     const { getByText } = render(<Display />);
+    //     expect(getByText.classList('lockedClass')).toBe(true)
+    // })
 })
